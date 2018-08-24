82d07fe91e40af2279e35ef6472fb729aac348d2
Zhijie Xu <jennyxu000@gmail.com>
2018-08-24 07:48:13 -0700

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Zhijie (Jenny) Xu on 8/24/18.
+//  Copyright © 2018 Zhijie (Jenny) Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
